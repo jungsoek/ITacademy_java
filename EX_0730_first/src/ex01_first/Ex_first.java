7091b66a74832fafc2264bd6489013c7b8d363ff
jungsoek <jungsoeko@gmail.com>
2024-07-30 18:54:26 +0900

    test:

---
+++ EX_0730_first/src/ex01_first/Ex_first.java
@@ -0,0 +1,21 @@
+package ex01_first;
+
+public class Ex_first {
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+	}
+}
+
+/* 범위 주석은
+ * 범위 안에 있으면
+ * 전부 주석 처리가 되는 주석
+ * */
+
+// Task Tag
+
+// TODO : 내일할일, 개선해야 할 점
+
+// FIXME : 해당 코드에서는 런타임 오류가 발생합니다. 수정해야 합니다.
+// 수정이 필요한 버그, 오류, 또는 잘못된 동작을 나타내는 주석태그
+// FIXME주석 태그를 포함하고 어떤 문제가 있는지 설명하고 이를 해결해야 하는 이유를 기록
+// 코드나 문서를 검토할 때 빠른 수정이 필요한 부분을 강조하는데 사용
---
+++ EX_0730_first/src/module-info.java
@@ -0,0 +1,2 @@
+module EX_0730_first {
+}
